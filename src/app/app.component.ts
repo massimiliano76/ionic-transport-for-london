b0aea186ee6121f29e79642b6b4000b60a1244f7
Thomas Clark <tnc1997@virginmedia.com>
2018-01-04 19:34:53 +0000

    refactor(home): add sidemenu to app and adjust colour scheme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-import { Platform } from 'ionic-angular';
+import { Nav, Platform } from 'ionic-angular';
+
+import { IPage } from '../models/page';
 
 @Component({
   templateUrl: 'app.html',
 })
 export class MyApp {
+  @ViewChild(Nav) public nav: Nav;
+
   public rootPage: any = 'HomePage';
+
+  public pages: IPage[];
 
   constructor(
       private platform: Platform,
       private statusBar: StatusBar,
       private splashScreen: SplashScreen,
   ) {
-    platform.ready().then(() => {
+    this.initializeApp();
+
+    this.pages = [
+      { component: 'HomePage', icon: 'home', title: 'Home' },
+    ];
+  }
+
+  public initializeApp() {
+    this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
     });
+  }
+
+  public openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.nav.setRoot(page.component);
   }
 }
---
+++ src/models/page.ts
@@ -0,0 +1,5 @@
+export interface IPage {
+  component: string;
+  icon: string;
+  title: string;
+}
